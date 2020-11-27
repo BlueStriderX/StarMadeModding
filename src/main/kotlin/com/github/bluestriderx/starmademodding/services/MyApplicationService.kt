07feb57f969a127f91577af93effad9892c5901a
GitHub Action <action@github.com>
2020-11-27 21:33:49 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/bluestriderx/starmademodding/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.bluestriderx.starmademodding.services
+
+import com.github.bluestriderx.starmademodding.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/bluestriderx/starmademodding/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bluestriderx/starmademodding/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.bluestriderx.starmademodding.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bluestriderx.starmademodding.MyBundle
 
 class MyProjectService(project: Project) {
 
